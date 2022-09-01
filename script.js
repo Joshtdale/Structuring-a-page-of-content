c78c4eb2e7be109598559c5192f4cec0481a1d37
Joshtdale <joshuadale65@gmail.com>
2022-09-01 22:04:00 +0000

    dark mode

--- script.js
+++ script.js
@@ -2,7 +2,28 @@ console.log('script working');
 
 let darkMode = document.getElementById('darkMode');
 
+const elements = document.querySelectorAll('.element')
 darkMode.addEventListener('click', () => {
     console.log('clicked')
-    if(document.body.style.backgroundColor)
+
+elements.forEach(element => {
+    element.style.backgroundColor = 'grey';
+    element.style.color = 'white';
+
+  });
+
+
+//    if(element.style.backgroundColor === '#00ff0080') {
+//      const element = document.getElementById('element')
+    
+// element.style.backgroundColor = 'grey';
+//    } else{
+//     element.style.backgroundColor = '#00ff0080'
+
+//    }
+
+
+
+    // if(document.html.style.backgroundColor === '#a9a9a9')
+    // document.main.style.backgroundColor = 'black'
 });
